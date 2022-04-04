954f85b0b0ec69764734744269c021fd4f59607c
a63114 <a63.114@yandex.ru>
2022-04-04 23:16:25 +0300

    Started new exercise

---
+++ For loop/49. Find the sum of an A.P. series.c
@@ -0,0 +1,44 @@
+// Copyright 2022 ficnhren
+/*
+Write a c program to find out the sum of an A.P. series
+Test Data :
+Input the starting number of the A.P. series: 1
+Input the number of items for the A.P. series: 10
+Input the common difference of A.P. series: 4
+Expected Output :
+The Sum of the A.P. series are :
+1 + 5 + 9 + 13 + 17 + 21 + 25 + 29 + 33 + 37 = 190
+
+1. Take input
+1.5 Invalid input
+2. Write a function to find elements
+3. Output sum
+4. Test
+5. Cpplint test
+6. Add and push
+*/
+
+#include <stdio.h>
+
+int input_start();
+int input_end();
+void print_invalid_input();
+
+int main() {
+    int start = input_start();
+    int end = input_end();
+    printf()
+    return 0;
+}
+
+void print_invalid_input() {
+    printf("n/a");
+}
+
+int input_start() {
+
+}
+
+int input_end() {
+
+}
