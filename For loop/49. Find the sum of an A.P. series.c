b31d0e7efab535ad68cd9b8962127b447f3d9c5f
a63114 <a63.114@yandex.ru>
2022-04-04 23:35:09 +0300

    Finished the exercise but no invalid input and no tests - goind to do them later

--- For loop/49. Find the sum of an A.P. series.c
+++ For loop/49. Find the sum of an A.P. series.c
@@ -9,36 +9,59 @@ Expected Output :
 The Sum of the A.P. series are :
 1 + 5 + 9 + 13 + 17 + 21 + 25 + 29 + 33 + 37 = 190
 
-1. Take input
-1.5 Invalid input
-2. Write a function to find elements
-3. Output sum
++ 1. Take input
+1.5 Invalid input - To be done later
++ 2. Write a function to find elements
++ 3. Output sum
 4. Test
-5. Cpplint test
-6. Add and push
++ 5. Cpplint test
++ 6. Add and push
 */
 
 #include <stdio.h>
 
 int input_start();
-int input_end();
-void print_invalid_input();
+int input_number_of_items();
+int input_difference();
+void find_and_print_result(int start, int number_of_items, int difference);
 
 int main() {
     int start = input_start();
-    int end = input_end();
-    printf()
+    int number_of_items = input_number_of_items();
+    int difference = input_difference();
+    find_and_print_result(start, number_of_items, difference);
     return 0;
 }
 
-void print_invalid_input() {
-    printf("n/a");
+void find_and_print_result(int start, int number_of_items, int difference) {
+    printf("The Sum of the A.P. series are:\n");
+    int sum = start, element = start;
+    printf("%d ", element);
+    for (int i = 0; i < number_of_items - 1; ++i) {
+        element += difference;
+        printf("+ %d ", element);
+        sum += element;
+    }
+    printf("= %d", sum);
 }
 
 int input_start() {
-
+    printf("Input the starting number of the A.P. series:\n");
+    int start;
+    scanf("%d", &start);
+    return start;
 }
 
-int input_end() {
+int input_number_of_items() {
+    printf("Input the number of items for the A.P. series:\n");
+    int number_of_items;
+    scanf("%d", &number_of_items);
+    return number_of_items;
+}
 
+int input_difference() {
+    printf("Input the common difference of A.P. series:\n");
+    int difference;
+    scanf("%d", &difference);
+    return difference;
 }
