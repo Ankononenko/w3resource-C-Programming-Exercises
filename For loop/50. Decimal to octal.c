ab1d7e44c6ac50713a766e6364efaf60ba9338b2
a63114 <a63.114@yandex.ru>
2022-04-05 22:46:48 +0300

    started octal

---
+++ For loop/50. Decimal to octal.c
@@ -0,0 +1,21 @@
+// Copyright 2022 finchren
+/*
+Write a program in C to convert a decimal number into octal without using an array
+Test Data :
+Enter a number to convert : 79
+Expected Output :
+The Octal of 79 is 117.
+
+1. Take input
+2. Figure out how to convert to octal
+3. Invalid input
+4. Convert
+5. Output
+6. Test
+7. Cpplint test
+8. Add and push
+*/
+
+int main() {
+    return 0;
+}
