f06711cc1284a43c866d6f75fa83ac26596f7077
a63114 <a63.114@yandex.ru>
2022-04-09 23:32:33 +0300

    Midway done with the exercise

--- For loop/50. Decimal to octal.c
+++ For loop/50. Decimal to octal.c
@@ -7,9 +7,24 @@ Expected Output :
 The Octal of 79 is 117.
 
 + 1. Take input
-2. Figure out how to convert to octal
++ 2. Figure out how to convert to octal:
+    Devide the decimal by 8 and take the quotient to devide until it's zero
+    Put that remainder at the end of the octal number;
+    79 % 8 == 7; quotient = 9
+    9 % 8 == 1; q = 1
+    1 % 8 == 1; q = 1
+    117
+    Conversion steps:
+        1 Divide the number by 8.
+        2 Get the integer quotient for the next iteration.
+        3 Get the remainder for the octal digit.
+        4 Repeat the steps until the quotient is equal to 0.
 3. Invalid input
-4. Convert
+4. Convert:
++    Function to count how many iterations of /8 there will be
++    Power function
+    Function to get the number int
+    Reverse it
 5. Output
 6. Test
 7. Cpplint test
@@ -19,10 +34,53 @@ The Octal of 79 is 117.
 #include <stdio.h>
 
 int input_decimal_number();
+int number_of_eights(int decimal_number);
+int power(int base, int exponent);
+int convert(int decimal_number);
 
 int main() {
     int decimal_number = input_decimal_number();
+    int division_by_eight = number_of_eights(decimal_number);
+    // int converted = convert(decimal_number);
+    printf("%d\n", division_by_eight);
+    printf("%d", power(5, 0));
     return 0;
+}
+// Conversion steps:
+//         1 Divide the number by 8.
+//         2 Get the integer quotient for the next iteration.
+//         3 Get the remainder for the octal digit.
+//         4 Repeat the steps until the quotient is equal to 0.
+// I will probably need a function to count how many iterations I will need to convert result into a number.
+// I'll need that function once again when I will flip the number
+int convert(int decimal_number) {
+    return decimal_number;
+}
+
+int power(int base, int exponent) {
+    int result;
+    if (exponent == 0) {
+        result = 1;
+    } else {
+        result = base;
+        for (int i = 2; i <= exponent; ++i) {
+            result *= base;
+        }
+    }
+    return result;
+}
+
+int number_of_eights(int decimal_number) {
+    int divisor = 8, quotient, counter = 0, temp_decimal = decimal_number;
+    for (int i = 0; i <= decimal_number; ++i) {
+        quotient = temp_decimal / divisor;
+        temp_decimal = quotient;
+        ++counter;
+        if (quotient == 0) {
+            break;
+        }
+    }
+    return counter;
 }
 
 int input_decimal_number() {
