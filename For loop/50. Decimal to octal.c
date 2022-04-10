0d5ed5e683f3704b0747b8000e934dc0f4b60bfa
a63114 <a63.114@yandex.ru>
2022-04-10 19:03:14 +0300

    Invalid input done. Need to figure out how to fix error with number that are almost at the end of int range

--- For loop/50. Decimal to octal.c
+++ For loop/50. Decimal to octal.c
@@ -19,7 +19,7 @@ The Octal of 79 is 117.
         2 Get the integer quotient for the next iteration.
         3 Get the remainder for the octal digit.
         4 Repeat the steps until the quotient is equal to 0.
-3. Invalid input
++ 3. Invalid input
 + 4. Convert:
 +    Function to count how many iterations of /8 there will be
 +    Power function
@@ -33,19 +33,74 @@ The Octal of 79 is 117.
 
 #include <stdio.h>
 
-int input_decimal_number();
+enum input_state {
+    FAILURE = 0,
+    SUCCESS = 1
+};
+
+enum is_negative {
+    TRUE = 1,
+    FALSE = 0
+};
+
+int input_decimal_number(int * decimal_number);
 int number_of_eights(int decimal_number);
 int power(int base, int exponent);
-int convert(int decimal_number, int division_by_eight);
+int convert(int decimal_number);
 int reverse_number(int converted_number, int division_by_eight);
 void print_octal(int decimal_number, int result);
+void print_invalid_input();
+void test(int number, int expected, int test_number);
+void run_tests();
 
 int main() {
-    int decimal_number = input_decimal_number();
-    int division_by_eight = number_of_eights(decimal_number);
-    int converted = convert(decimal_number, division_by_eight);
-    print_octal(decimal_number, converted);
+    // int decimal_number = 0;
+    // int input_state = input_decimal_number(&decimal_number);
+    // if (input_state == SUCCESS) {
+    //     int converted = convert(decimal_number);
+    //     print_octal(decimal_number, converted);
+    // } else {
+    //     print_invalid_input();
+    // }
+    run_tests();
     return 0;
+}
+
+void run_tests() {
+    /*
+    // Normal value tests
+    test(79, 117, 1);
+    test(1, 1, 2);
+    test(15, 17, 3);
+    test(555, 1053, 4);
+    test(8888, 21270, 5);
+    // Negative number test
+    test(-79, -117, 6);
+    test(-1, -1, 7);
+    test(-15, -17, 8);
+    test(-555, -1053, 9);
+    test(-8888, -21270, 10);
+    // Zero test
+    test(0, 0, 11);
+    // Big number test
+    test(47483647, 265105377, 12);
+    */
+    // Near end of int range
+    test(294903430, 2144757206, 13);
+}
+
+void test(int number, int expected, int test_number) {
+    int actual = convert(number);
+    if (actual == expected) {
+        printf("Test #%d passed\n", test_number);
+    } else {
+        printf("Test #%d failed. Number = %d, expected = %d, actual = %d\n",
+        test_number, number, expected, actual);
+    }
+}
+
+void print_invalid_input() {
+    printf("n/a");
 }
 
 void print_octal(int decimal_number, int result) {
@@ -78,7 +133,13 @@ I will probably need a function to count how many iterations I will need to conv
 I'll need that function once again when I will flip the number
 */
 
-int convert(int decimal_number, int division_by_eight) {
+int convert(int decimal_number) {
+    int is_negative = FALSE;
+    if (decimal_number < 0) {
+        decimal_number *= -1;
+        is_negative = TRUE;
+    }
+    int division_by_eight = number_of_eights(decimal_number);
     int result = 0, quotient, remainder, temp_decimal = decimal_number,
     divider = 8, power_counter = division_by_eight;
     for (int i = 0; i <= division_by_eight; ++i) {
@@ -93,6 +154,9 @@ int convert(int decimal_number, int division_by_eight) {
         temp_decimal = quotient;
     }
     result = reverse_number(result, division_by_eight);
+    if (is_negative == TRUE) {
+        result *= -1;
+    }
     return result;
 }
 
@@ -122,8 +186,13 @@ int number_of_eights(int decimal_number) {
     return counter;
 }
 
-int input_decimal_number() {
-    int decimal_number;
-    scanf("%d", &decimal_number);
-    return decimal_number;
+int input_decimal_number(int * decimal_number) {
+    int input_state = FAILURE;
+    char endline = '\0';
+    if (!scanf("%d%c", decimal_number, &endline) || endline != '\n') {
+        input_state = FAILURE;
+    } else {
+        input_state = SUCCESS;
+    }
+    return input_state;
 }
