cc4915bb380632a070b1cb8085a6a43d2f74c1b8
a63114 <a63.114@yandex.ru>
2022-04-13 23:14:32 +0300

    Input and invalid input done

--- For loop/51. Octal to decimal.c
+++ For loop/51. Octal to decimal.c
@@ -9,19 +9,45 @@ The equivalent Decimal Number : 485
 
 + 1. Figure out howo to convert octal to decimal
     745 = (7 * 8^2) + (4 * 8^1) + (5 * 8^0) = 485
-1.5 Input
++ 1.5 Input
 2. Pow function
 3. Function to count amount of digits
 4. Write function to iterate over digits and return sum of powers
-5. Invalid input
++ 5. Invalid input
 6. Test
 7. Cpplint test
 8. Add and push 
 */
 
 #include <stdio.h>
-  
+
+enum boolean_for_validation {
+    TRUE = 1,
+    FALSE = 0
+};
+
+int input_octal(int *octal);
+void print_invalid_input();
+
 int main() {
-   
+   int octal = 0;
+   if (input_octal(&octal)) {
+        printf("%d", octal);
+   } else {
+       print_invalid_input();
+   }
    return 0;
+}
+
+void print_invalid_input() {
+    printf("n/a");
+}
+
+int input_octal(int *octal) {
+    int is_valid = TRUE;
+    char endline = '\0';
+    if (!scanf("%d%c", octal, &endline) || endline != '\n') {
+        is_valid = FALSE;
+    }
+    return is_valid;
 }
