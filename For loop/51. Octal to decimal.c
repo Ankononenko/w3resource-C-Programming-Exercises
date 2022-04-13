4cc787848b0724387d92aa892d360898a4101b73
a63114 <a63.114@yandex.ru>
2022-04-13 23:06:26 +0300

    wrote a plan to solve the exercise

--- For loop/51. Octal to decimal.c
+++ For loop/51. Octal to decimal.c
@@ -6,19 +6,22 @@ Input an octal number (using digit 0 - 7) :745
 Expected Output :
 The Octal Number : 745
 The equivalent Decimal Number : 485
+
++ 1. Figure out howo to convert octal to decimal
+    745 = (7 * 8^2) + (4 * 8^1) + (5 * 8^0) = 485
+1.5 Input
+2. Pow function
+3. Function to count amount of digits
+4. Write function to iterate over digits and return sum of powers
+5. Invalid input
+6. Test
+7. Cpplint test
+8. Add and push 
 */
 
 #include <stdio.h>
-
-// Enum test
-enum State {WORKING = 0, FAILED, FREEZED};
-enum State currState = 2;
-  
-enum State FindState() {
-    return currState;
-}
   
 int main() {
-   (FindState() == WORKING)? printf("WORKING"): printf("NOT WORKING");
+   
    return 0;
 }
