9c748f8b61faf9ec93d359a760cf867c53f2a7af
a63114 <a63.114@yandex.ru>
2022-04-17 01:17:50 +0300

    Started binary to octal

---
+++ For loop/53. Binary number to octal.c
@@ -0,0 +1,16 @@
+// Copyright 2022 finchren
+/*
+Write a program in C to convert a binary number to octal
+Test Data :
+Input a binary number :1001
+Expected Output :
+The Binary Number : 1001
+The equivalent Octal Number : 11
+*/
+
+#include <stdio.h>
+
+int main() {
+
+    return 0;
+}
