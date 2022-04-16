7435d3fa930bb46d4d6e4c8ecada8b1a0932e480
a63114 <a63.114@yandex.ru>
2022-04-16 23:15:47 +0300

    Started exercise 52

---
+++ For loop/52. Sum of GP series.c
@@ -0,0 +1,26 @@
+// Copyright 2022 finchren
+/*
+Write a program in c to find the Sum of GP series
+Test Data :
+Input the first number of the G.P. series: 3
+Input the number or terms in the G.P. series: 5
+Input the common ratio of G.P. series: 2
+Expected Output :
+The numbers for the G.P. series:
+3.000000 6.000000 12.000000 24.000000 48.000000
+The Sum of the G.P. series : 93.000000
+
+1. Write tests (run tests function + body of a test)
+2. Input
+3. Invalid input
+4. Finction to print out and find the sum
+5. Check the tests
+6. Cppcheck
+7. Add and push
+*/
+
+#include <stdio.h>
+
+int main() {
+    return 0;
+}
