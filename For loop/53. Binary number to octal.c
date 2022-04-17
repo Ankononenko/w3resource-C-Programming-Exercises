1c44a9bff3d7e150f47d7a8e992de4a2515cd3dd
a63114 <a63.114@yandex.ru>
2022-04-17 15:14:28 +0300

    Input finished

--- For loop/53. Binary number to octal.c
+++ For loop/53. Binary number to octal.c
@@ -6,11 +6,88 @@ Input a binary number :1001
 Expected Output :
 The Binary Number : 1001
 The equivalent Octal Number : 11
+
++ 1. Figure out how to convert binary straight to octal
+At this moment I can binary -> decimal -> octal. There probably is a better way to do this:
+Convert every 3 binary digits (start from bit 0) to 1 octal digit, with binary table from 0 to 7
+Ex: 00001100010001 = 00 001 100 010 001 = 1 4 2 1 = 1421
+2. Input a dinamic array of chars
+3. Convertion: 
+    Enum of binary valies from 0 to 7
+    Iterate over an array, assign each pair of three from the end of the array octal value
+    Output the value
+4. Invalid input
+5. Test
+6. Cpplint test
+7. Add and push
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+
+enum is_valid{
+    TRUE = 1,
+    FALSE = 0
+};
+
+int input_binary(char *pointer_to_binary_array);
+void print_invalid_input();
+void print_binary_array(char *pointer_to_binary_array);
 
 int main() {
-
+    char *pointer;
+    pointer = (char*)malloc(1 * sizeof(char));
+    if (pointer == NULL) {
+        printf("Memory could not be allocated");
+    } else {
+        int is_valid = input_binary(pointer);
+        print_binary_array(pointer);
+        if (is_valid == FALSE) {
+            print_invalid_input();
+        }
+    }
     return 0;
+}
+
+void print_binary_array(char *pointer_to_binary_array) {
+    for (int i = 0; ;++i) {
+        if (pointer_to_binary_array[i] == '\0') {
+            break;
+        } 
+        printf("%c", pointer_to_binary_array[i]);
+    }
+}
+
+void print_invalid_input() {
+    printf("n/a");
+}
+
+int input_binary(char *pointer_to_binary_array) {
+    int array_lenght = 1, is_valid = TRUE;
+    char endline = '\n', temp_char = '\0';
+    for (int i = 0; i < array_lenght; ++i) {
+        if(!scanf("%c", &temp_char)) {
+            if (pointer_to_binary_array) {
+                free(pointer_to_binary_array);
+                pointer_to_binary_array = NULL;
+            }
+            is_valid = FALSE;
+        } else {
+            if (temp_char == endline) {
+                pointer_to_binary_array[i] = '\0';
+                break;
+            }
+            if (temp_char != '1' && temp_char != '0') {
+                is_valid = FALSE;
+                if (pointer_to_binary_array) {
+                free(pointer_to_binary_array);
+                pointer_to_binary_array = NULL;
+                }
+            }
+            pointer_to_binary_array[i] = temp_char;
+            ++array_lenght;
+            pointer_to_binary_array = (char*)realloc(pointer_to_binary_array, (array_lenght)*sizeof(char));
+        }
+    }
+    return is_valid;
 }
