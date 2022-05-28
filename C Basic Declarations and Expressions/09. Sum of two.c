ba4aad3b22fd9d57cd4b4e5bd4de696f86c732a8
163114 <88505337+163114@users.noreply.github.com>
2022-05-28 10:44:34 +0300

    Another easy exercise for practice - macro func

---
+++ C Basic Declarations and Expressions/09. Sum of two.c
@@ -0,0 +1,65 @@
+/*
+Write a C program that accepts two integers from the user and calculate the sum of the two integers
+Test Data :
+Input the first integer: 25
+Input the second integer: 38
+Expected Output:
+Sum of the above two integers = 63
+
++ 1. Function for the first number
++ 2. Function for the second number
++ 3. Invalid input
++ 4. Sum function
++ 5. Output the result
++ 6. Test
++ 7. Cpplint test
+8. Add and push
+*/
+
+#include <stdio.h>
+#define SUM_OF_TWO(first, second) (first) + (second)
+
+enum is_valid_value {
+    FALSE,
+    TRUE
+};
+
+int input_number(double *number);
+void print_invalid_input();
+void print_result(double sum);
+
+int main() {
+    double first_number = 0.0;
+    printf("Input the first integer: ");
+    if (!input_number(&first_number)) {
+        print_invalid_input();
+    } else {
+        double second_number = 0.0;
+        printf("Input the second integer: ");
+        if (!input_number(&second_number)) {
+            print_invalid_input();
+        } else {
+            double sum = SUM_OF_TWO(first_number, second_number);
+            print_result(sum);
+            }
+    }
+    return 0;
+}
+
+int input_number(double *number) {
+    int is_valid_input = TRUE;
+    char endline = '\n';
+    if (!scanf("%lf%c", number, &endline) || endline != '\n') {
+        is_valid_input = FALSE;
+    }
+    return is_valid_input;
+}
+
+void print_invalid_input() {
+    printf("Invalid input\n"
+    "The program takes only one double value per iteration\n");
+}
+
+void print_result(double sum) {
+    printf("Sum of the above two integers = %lf", sum);
+}
